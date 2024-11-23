8ff8a3637a93f1f9bad4a0646e42eae109aa18d3
pvpmastero4ek9 <at@panacotagames.com>
2024-11-23 11:59:46 +0300

    Добавил таймер

--- Assets/_Project/Core/Bar/UsuallyBar.cs
+++ Assets/_Project/Core/Bar/UsuallyBar.cs
@@ -5,7 +5,6 @@ namespace Core.Bars
     public class UsuallyBar : Bar
     {
         public UsuallyBar(int maxValue, int initialValue) : base(maxValue, initialValue)
-        {
-        }
+        {}
     }
 }
